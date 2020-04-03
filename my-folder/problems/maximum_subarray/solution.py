f35dc42bd8d717703fcd1736eeb104e7f67a72ad
Shubham Wagh <shubhamwagh48@gmail.com>
2020-04-03 15:07:11 +0000

    Sync LeetCode submission - Maximum Subarray (python3)

---
+++ my-folder/problems/maximum_subarray/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        #Kadane's algorithm
+        max_current = max_global = nums[0]
+        
+        for ind in range(1, len(nums)):
+            max_current = max(nums[ind], max_current + nums[ind])
+            if max_current > max_global:
+                max_global = max_current
+        
+        return max_global
+        
