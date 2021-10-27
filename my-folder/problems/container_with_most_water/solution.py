95a5c2931f4fb864bb1c7e9cff1fda38e9de0a17
Shubham Wagh <shubhamwagh48@gmail.com>
2021-10-27 12:52:16 +0000

    Sync LeetCode submission - Container With Most Water (python3)

---
+++ my-folder/problems/container_with_most_water/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = 0
+        pointer_one = 0
+        pointer_two = len(height) - 1
+        
+        
+        while pointer_one < pointer_two:
+            w = pointer_two - pointer_one
+            h = min(height[pointer_one], height[pointer_two])
+            area = w * h
+            max_area = max(area, max_area)
+            
+            if height[pointer_one] < height[pointer_two]:
+                pointer_one += 1
+            else:
+                pointer_two -= 1
+                
+        return max_area        
+        
