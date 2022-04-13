9341d1c0ee9334185dcaf0e87b5f6e60b1e35292
Shubham Wagh <shubhamwagh48@gmail.com>
2022-04-13 15:06:28 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- my-folder/problems/number_of_islands/solution.py
+++ my-folder/problems/number_of_islands/solution.py
@@ -1,24 +1,32 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0 
+        visited = set()
+        for row in range(0, len(grid)):
+            for col in range(0, len(grid[0])):
+                if self._explore_island(grid, row, col, visited):
+                    count += 1
+        return count
         
-        if not grid:
-            return 0
+    def _explore_island(self, grid, row, col, visited):
+        row_in_bounds = 0 <= row < len(grid)
+        col_in_bounds = 0 <= col < len(grid[0])
         
-        num_island = 0
+        if not row_in_bounds or not col_in_bounds:
+            return False
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1":   
-                    self.search_neighbour_cells(grid, i, j)
-                    num_island +=1
-        return num_island
-    
-    def search_neighbour_cells(self, grid, i, j):
-        if i < 0 or i >= len(grid) or j< 0 or j>=len(grid[0]) or grid[i][j]!="1":
-            return
-        grid[i][j] = "0"
-        self.search_neighbour_cells(grid, i, j+1)
-        self.search_neighbour_cells(grid, i, j-1)
-        self.search_neighbour_cells(grid, i+1, j)
-        self.search_neighbour_cells(grid, i-1, j)
+        if grid[row][col] == '0':
+            return False
         
+        key = str(row) + ',' + str(col)
+        if key in visited:
+            return False
+        
+        visited.add(key)
+        
+        neighbours = [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
+        
+        for r, c in neighbours:
+            self._explore_island(grid, r, c, visited)
+        
+        return True
