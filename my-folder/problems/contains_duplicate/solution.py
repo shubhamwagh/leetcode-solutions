1dcf1e89fae5643168fb11ef340d2f311b45b161
Shubham Wagh <shubhamwagh48@gmail.com>
2023-11-14 17:39:42 +0000

    Delete my-folder/problems directory

--- my-folder/problems/contains_duplicate/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        hash_set = set()
-
-        for num in nums:
-            if num in hash_set:
-                return True
-            hash_set.add(num)
-        return False  
--- my-folder/problems/squares_of_a_sorted_array/solution.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        sorted_num = sorted(nums, key=lambda x: abs(x))
-        out = [x**2 for x in sorted_num]
-        return out
-        
-        
